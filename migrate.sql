fee222e94dfe6af1d766598571c8d5a3b40a7260
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-01 20:49:00 +0700

    migrate

--- migrate.sql
+++ migrate.sql
@@ -29,6 +29,7 @@ create table products (
     description text,
     brandID int,
     price int,
+    sell_quantity int,
     pic1 varchar(40),
     pic2 varchar(40),
     pic3 varchar(40),
@@ -39,23 +40,36 @@ create table products (
 
 CREATE TABLE comments(
     commentID int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(40),
+    email varchar(100),
+    subject varchar(100),
+    message text,
     productID int,
-    username varchar(32),
-    content text,
-    FOREIGN KEY (productID) REFERENCES products(productID),
-    FOREIGN KEY (username) REFERENCES users(username)
-);
-
-CREATE TABLE articles(
-    articleID int AUTO_INCREMENT PRIMARY KEY,
-    title varchar(100),
-    body text
-);
-
-CREATE TABLE article_product(
-    ID int AUTO_INCREMENT PRIMARY KEY,
-    articleID int,
-    productID int,
-    FOREIGN KEY (articleID) REFERENCES articles(articleID),
     FOREIGN KEY (productID) REFERENCES products(productID)
-)
+);
+
+create table contact(
+    contactID int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(40),
+    email varchar(100),
+    subject varchar(100),
+    message text
+);
+
+create table user_order(
+    name varchar(40),
+    ordernumber int AUTO_INCREMENT PRIMARY KEY,
+    email varchar(100),
+    phone int(10),
+    address varchar(200),
+    paymethod int
+);
+
+CREATE TABLE orders(
+    orderID int AUTO_INCREMENT PRIMARY KEY,
+    ordernumber int NOT NULL,
+    productID int,
+    quantity int,
+    price int,
+    FOREIGN KEY (ordernumber) REFERENCES user_order(ordernumber)
+);

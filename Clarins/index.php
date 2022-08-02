1917b900ae88b504b4328299449a813152f466cd
ngan181 <ngan.nt.2020@aptechlearning.edu.vn>
2022-08-02 22:38:48 +0700

    Create index.php

---
+++ Clarins/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CLARINS</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary py-1 d-none d-md-block">
+        <div class="container">
+            <div class="text-center">
+                <img src="skin-care.png" style="max-width:10%;">
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
+                <a href="index.php" class="navbar-brand d-block d-lg-none">
+                    <h1 class="m-0 display-4 text-primary"><span class="text-secondary">C</span>LARINS</h1>
+
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                        <a href="about.php" class="nav-item nav-link">About</a>
+                        <a href="product.php" class="nav-item nav-link">Products</a>
+                    </div>
+                        <a href="index.php" class="navbar-brand mx-5 d-none d-lg-block">
+                            <h1 class="m-0 display-4 text-primary"><span class="text-secondary">C</span>LARINS</h1>
+                        </a> 
+                    <div class="navbar-nav mr-auto py-0">
+                        <div class="dropdown">
+                            <a href="#" class="nav-item nav-link dropdown-toggle" data-bs-toggle="dropdown">Gallery</a>
+                            <div class="dropdown-menu">
+                                <div class="dropdown-header">FACE</div>
+                                <a href="#" class="dropdown-item">Cleaners&Toners</a>
+                                <a href="#" class="dropdown-item">Serums</a>
+                                <a href="#" class="dropdown-item">Day care</a>
+                                <a href="#" class="dropdown-item">Night care</a>
+                                <div class="dropdown-header">BODY</div>
+                                <a href="#" class="dropdown-item">Hand&Foot care</a>
+                                <a href="#" class="dropdown-item">Bath&Shower</a>
+                                <a href="#" class="dropdown-item">Body Firming</a>
+                                <a href="#" class="dropdown-item">Body Contouring</a>
+                                <div class="dropdown-header">SUNSCREEN</div>
+                                <a href="#" class="dropdown-item">Sun Protection</a>
+                                <a href="#" class="dropdown-item">After Sun</a>
+                                <div class="dropdown-header">HAIR</div>
+                                <div class="dropdown-header">MAKEUP</div>
+                                <div class="dropdown-header">PERFUMES</div>
+                            </div>
+                        </div>
+                        <a href="index.php" class="nav-item nav-link">Service</a>
+                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5 pb-5">
+        <div id="header-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/natural2.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h4 class="text-white text-uppercase mb-md-3">SUSTAINABLE COMMITMENT</h4>
+                            <h1 class="display-3 text-white mb-md-4">With A Wide Variety Of Products</h1>
+                            <a href="product.php" class="btn btn-primary py-md-3 px-md-5 mt-2">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="img/natural.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h4 class="text-white text-uppercase mb-md-3">SUSTAINABLE COMMITMENT</h4>
+                            <h1 class="display-3 text-white mb-md-4">Made From Natural Ingredients</h1>
+                            <a href="product.php" class="btn btn-primary py-md-3 px-md-5 mt-2">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                <div class="btn btn-secondary px-0" style="width: 45px; height: 45px;">
+                    <span class="carousel-control-prev-icon mb-n1"></span>
+                </div>
+            </a>
+            <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+                <div class="btn btn-secondary px-0" style="width: 45px; height: 45px;">
+                    <span class="carousel-control-next-icon mb-n1"></span>
+                </div>
+            </a>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+    <!-- About Start -->
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <h1 class="section-title position-relative text-center mb-5">SUSTAINABLE COMMITMENT Sustainable commitment to product quality</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 py-5">
+                    <h4 class="font-weight-bold mb-3">About Us</h4>
+                    <p>We are proud to bear the B Corp ™ certification, recognized as one of 3,000 businesses worldwide with the highest social and environmental standards, and that drives us to a commitment to sustainability. , starting with packaging from Community Trade Recycled Plastics.</p>
+                    <a href="about.php" class="btn btn-secondary mt-2">Learn More</a>
+                </div>
+                <div class="col-lg-4" style="min-height: 400px;">
+                    <div class="position-relative h-100 rounded overflow-hidden">
+                        <img class="position-absolute w-100 h-100" src="img/picture.jpg" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-4 py-5">
+                    <h4 class="font-weight-bold mb-3">Our Features</h4>
+                    <p>This French brand was established in 1954, specializing in the production of high-class skin care products and makeup cosmetics, extracted mainly from plants.</p>
+                    <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>healthy products</h5>
+                    <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>clear ingredients</h5>
+                    <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>high efficiency</h5>
+                    <a href="product.php" class="btn btn-primary mt-2">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+
+    <!-- Promotion Start -->
+    <div class="container-fluid my-5 py-5 px-0">
+        <div class="row bg-primary m-0">
+            <div class="col-md-6 px-0" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100" src="img/order.jpg" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-md-6 py-5 py-md-0 px-0">
+                <div class="h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                    <div class="d-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                        style="width: 100px; height: 100px;">
+                        <h3 class="font-weight-bold text-secondary mb-0">$199</h3>
+                    </div>
+                    <h3 class="font-weight-bold text-white mt-3 mb-4">"Love the texture"</h3>
+                    <p class="text-white mb-4">I started using this a month ago, and I have seen great results. 
+                        I love the texture of the serum and I love the fact that its very smooth and light.</p>
+                    <a href="product.php" class="btn btn-secondary py-3 px-5 mt-2">Order Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Promotion End -->
+
+
+    <!-- Video Modal Start -->
+    <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>        
+                     16:9 aspect ratio 
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Video Modal End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-secondary px-2 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+<?php
+    include('footer.php');
+?>

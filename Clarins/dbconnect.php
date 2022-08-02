67d9d4552e898da1efa87b9eabb5abf09bfd0571
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-02 22:00:27 +0700

    add visitor count

--- Clarins/dbconnect.php
+++ Clarins/dbconnect.php
@@ -7,3 +7,10 @@ $conn = new mysqli($host, $user, $psw, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+function output($output)
+{
+    echo ("<pre>");
+    echo (var_dump($output));
+    echo ("</pre>");
+}

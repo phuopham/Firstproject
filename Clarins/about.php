4441fbfd27e354a925b7ddca500dfa1f8b981136
ngan181 <ngan.nt.2020@aptechlearning.edu.vn>
2022-08-02 22:37:49 +0700

    Create about.php

---
+++ Clarins/about.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CLARINS</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+        <!-- Topbar Start -->
+        <div class="container-fluid bg-primary py-1 d-none d-md-block">
+        <div class="container">
+            <div class="text-center">
+                <img src="skin-care.png" style="max-width:10%;">
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
+                <a href="index.html" class="navbar-brand d-block d-lg-none">
+                    <h1 class="m-0 display-4 text-primary"><span class="text-secondary">C</span>LARINS</h1>
+
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="about.php" class="nav-item nav-link active">About</a>
+                        <a href="index.php" class="nav-item nav-link">Products</a>
+                    </div>
+                        <a href="index.php" class="navbar-brand mx-5 d-none d-lg-block">
+                            <h1 class="m-0 display-4 text-primary"><span class="text-secondary">C</span>LARINS</h1>
+                        </a> 
+                    <div class="navbar-nav mr-auto py-0">
+                        <div class="dropdown">
+                            <a href="#" class="nav-item nav-link dropdown-toggle" data-bs-toggle="dropdown">Gallery</a>
+                            <div class="dropdown-menu">
+                                <div class="dropdown-header">FACE</div>
+                                <a href="#" class="dropdown-item">Cleaners&Toners</a>
+                                <a href="#" class="dropdown-item">Serums</a>
+                                <a href="#" class="dropdown-item">Day care</a>
+                                <a href="#" class="dropdown-item">Night care</a>
+                                <div class="dropdown-header">BODY</div>
+                                <a href="#" class="dropdown-item">Hand&Foot care</a>
+                                <a href="#" class="dropdown-item">Bath&Shower</a>
+                                <a href="#" class="dropdown-item">Body Firming</a>
+                                <a href="#" class="dropdown-item">Body Contouring</a>
+                                <div class="dropdown-header">SUNSCREEN</div>
+                                <a href="#" class="dropdown-item">Sun Protection</a>
+                                <a href="#" class="dropdown-item">After Sun</a>
+                                <div class="dropdown-header">HAIR</div>
+                                <div class="dropdown-header">MAKEUP</div>
+                                <div class="dropdown-header">PERFUMES</div>
+                            </div>
+                        </div>
+                        <a href="index.php" class="nav-item nav-link">Service</a>
+                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+    <!-- About Start -->
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <h1 class="section-title position-relative text-center mb-5">Established History</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 py-5">
+                    <p>The company was founded in 1954 by a medical student Jacques Courtin-Clarins
+                        The company founder's son, Christian Courtin-Clarins, has been Clarins' chairman since 2000.
+                        In 1957, Courtin-Clarins developed a new approach to face and body treatments and created a number of plant-based luxury products.
+                        In the early 1970s, the company began to expand worldwide with the first exclusive contracts.
+                    </p>
+                </div>
+                <div class="col-lg-4" style="min-height: 400px;">
+                    <div class="position-relative h-100 rounded overflow-hidden">
+                        <img class="position-absolute w-100 h-100" src="img/order.jpg" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-4 py-5">
+                    <p>The first US subsidiary was founded in January 1981. But first established in 1987 as Clarins sunscreen, this was really the company's start. 
+                        At the end of 2005, the group had 19 subsidiaries distributed in more than 150 countries. Serge Rosinoer, who died in 2014, is noted as an international developer.
+                        Virginie Courtin-Clarins, Claire Courtin-Clarins, Prisca Courtin-Clarins and Jenna Courtin-Clarins act as brand ambassadors.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+    <section>
+        <div class="container-fluid footer bg-light py-5" style="margin-top: 30px;">
+            <div class="container text-center py-5">
+                <div>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-6">
+                            <h1 class="section-title position-relative text-center mb-5">Contact Method</h1>
+                        </div>
+                    </div>
+                    <div style="display:flex; flex-wrap:wrap;">
+                        <div class="col-4">
+                            <div class="text-center text-sm-left border-right mb-3 mb-sm-5">
+                                <h5 class="font-weight-bold mb-3">Get In Touch</h5>
+                                <h5 class=" mb-3">Visit one of our agency locations or contact us today</h5>
+                            </div>
+                            <div class="text-center text-sm-left">
+                                <h5 class="font-weight-bold mb-3">Head Office</h5>
+                                <p class="mb-2">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    285 Doi Can, Ba Dinh, Ha Noi, Viet Nam</p>
+                                <p class="mb-0">
+                                    <i class="fas fa-phone"></i>
+                                    +012 345 67890
+                                </p>
+                                <p class="mb-2">
+                                    <i class="fas fa-clock"></i>
+                                    Mon – Sat, 8AM – 5PM
+                                </p>
+                                <p class="mb-0">Sunday: Closed</p>
+                            </div>
+                        </div>
+                        <div class="col-8 text-center text-sm-right">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.924278651594!2d105.81684051396614!3d21.035715592918905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab145bf89bd7%3A0xd94a869b494c04b6!2zMjg1IMSQ4buZaSBD4bqlbiwgVsSpbmggUGjDuiwgQmEgxJDDrG5oLCBIw6AgTuG7mWkgMTAwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1653971874345!5m2!1svi!2s"
+                                style="border: 0;max-width: 100%;border: 0;width: -webkit-fill-available;height: -webkit-fill-available;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Promotion Start -->
+        <div class="container-fluid my-5 py-5 px-0">
+            <div class="row bg-primary m-0">
+                <div class="col-md-6 px-0" style="min-height: 500px;">
+                    <div class="position-relative h-100">
+                        <img class="position-absolute w-100 h-100" src="img/aboutus.jpg" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-md-6 py-5 py-md-0 px-0">
+                    <div class="h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                        <h3 class="font-weight-bold text-white mt-3 mb-4">ABOUT CLARINS</h3>
+                        <p class="text-white mb-4">Clarins is a popular makeup and skincare brand. Featured over and over again in huge publications like Forbes, Vogue, Yahoo, Glamour, Bustle, and more, it’s safe to say that the brand is internationally successful. 
+                            This Clarins review will show you some highlights of the shopping experience, discuss some bestsellers, customer feedback, and answers to FAQs, all in the hopes of helping you decide if this brand fits your vibe.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Promotion End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-secondary px-2 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+<?php
+    include('footer.php');
+?>

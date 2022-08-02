9d85d8703d44ef85b9f8e28fb88a051c18c16b44
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-02 11:58:49 +0700

    add cart form and dbconnect
    add cart form and dbconnect

---
+++ Clarins/cart.php
@@ -0,0 +1,54 @@
+<?php
+include("header.php");
+
+$items = [1, 2, 3]
+
+?>
+
+<!-- About Start -->
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-8 py-5">
+                <h4 class="font-weight-bold mb-3">Cart</h4>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($items as $key => $item) {
+                            echo ("<tr>");
+                            echo ("<td>$key</td>");
+                            echo ("<td>$item</td>");
+                            echo ("<td>$item</td>");
+                            echo ("<td>$item</td>");
+                            echo ("<td>$item</td>");
+                            echo ("</tr>");
+                        };
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-4 py-5">
+                <h4 class="font-weight-bold mb-3">Our Features</h4>
+                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor</p>
+                <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>Eos kasd eos dolor</h5>
+                <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>Eos kasd eos dolor</h5>
+                <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>Eos kasd eos dolor</h5>
+                <a href="" class="btn btn-primary mt-2">Check out</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+<?php
+include("footer.php");
+?>

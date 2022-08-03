7684a120a6a713eb613482153822ed24043c71db
MinhHieu03 <108691518+MinhHieu03@users.noreply.github.com>
2022-08-03 19:42:24 +0700

    new login

--- Clarins/loginweb.php
+++ Clarins/loginweb.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>AdminLTE 3 | Log in</title>
-      
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-        <!-- icheck bootstrap -->
-        <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-      </head>
-</head>
+<?php
+require_once("dbconnect.php");
+
 <body>
     <body class="login-page" style="min-height: 496.8px;">
         <div class="login-box">
@@ -92,4 +77,3 @@
         <script src="../../dist/js/adminlte.min.js"></script>
         
 </body>
-</html>

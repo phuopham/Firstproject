bd6cfeee136538b079a329e63dcc66d06af0509a
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-04 23:49:12 +0700

    add config file

---
+++ Clarins/config.php
@@ -0,0 +1,9 @@
+<?php
+//On page about // paragraph on the left
+
+$about_paragraph1 = "The company was founded in 1954 by a medical student Jacques Courtin-Clarins
+The company founder's son, Christian Courtin-Clarins, has been Clarins' chairman since 2000.
+In 1957, Courtin-Clarins developed a new approach to face and body treatments and created a number of plant-based luxury products.
+In the early 1970s, the company began to expand worldwide with the first exclusive contracts.";
+
+$footer_opentime = "Mon – Sat, 8AM – 5PM";

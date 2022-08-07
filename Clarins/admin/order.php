ff27a714db5d3a7a0f0caf305963e60a522b97a4
ngan181 <ngan.nt.2020@aptechlearning.edu.vn>
2022-08-07 22:28:17 +0700

    Update order.php

--- Clarins/admin/order.php
+++ Clarins/admin/order.php
@@ -4,10 +4,12 @@ include("header.php");
 
 // get user data
 require_once("../dbconnect.php");
-$sql = "SELECT * from user_order";
-$result = $conn->query($sql);
-$userlist = $result->fetch_all(MYSQLI_ASSOC);
-$sql = "SELECT * from orders inner join user_order ON orders.ordernumber = user_order.ordernumber";
+// $sql = "SELECT * from user_order";
+// $result = $conn->query($sql);
+// $userlist = $result->fetch_all(MYSQLI_ASSOC);
+$sql = "SELECT user_order.name, user_order.ordernumber, user_order.email,user_order.phone,user_order.address, 
+orders.productID, orders.quantity,orders.price,orders.create_by  from orders inner join user_order 
+ON orders.ordernumber = user_order.ordernumber";
 $result = $conn->query($sql);
 $orderlist = $result->fetch_all(MYSQLI_ASSOC);
 //get user data end
@@ -49,8 +51,6 @@ $orderlist = $result->fetch_all(MYSQLI_ASSOC);
                                         <th>Email</th>
                                         <th>Phone</th>
                                         <th>Address</th>
-                                        <th>Pay Method</th>
-                                        <th>Order ID</th>
                                         <th>Product ID</th>
                                         <th>Quantity</th>
                                         <th>Price</th>
@@ -59,18 +59,14 @@ $orderlist = $result->fetch_all(MYSQLI_ASSOC);
                                 </thead>
                                 <tbody>
                                     <?php
-                                    foreach($userlist as $id => $user ){
+                                    foreach($orderlist as $id => $order){
                                         echo ("<tr>");
                                         echo ("<td>" . $user["name"] . "</td>");
                                         echo ("<td>" . ($id + 1) . "</td>");
                                         echo ("<td>" . $user["email"] . "</td>");
                                         echo ("<td>" . $user["phone"] . "</td>");
                                         echo ("<td>" . $user["address"] . "</td>");
-                                        echo ("<td>" . $user["paymethod"] . "</td>");
-                                    };
-                                    foreach($orderlist as $id_od => $order){
-                                        echo ("<td>" . ($id_od + 1) . "</td>");
-                                        echo ("<td>" . ($id_od + 1) . "</td>");
+                                        echo ("<td>" . ($id + 1) . "</td>");
                                         echo ("<td>" . $order["quantity"] . "</td>");
                                         echo ("<td>" . $order["price"] . "</td>");
                                         echo ("<td>" . $order["create_by"] . "</td>");

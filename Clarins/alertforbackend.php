1008810d6338d16cdc9d15ef4671710464006695
MinhHieu03 <108691518+MinhHieu03@users.noreply.github.com>
2022-08-12 09:42:16 +0700

    Create alertforbackend.php

---
+++ Clarins/alertforbackend.php
@@ -0,0 +1,31 @@
+//js
+Swal.fire(
+  'Good job!',
+  'You clicked the button!',
+  'success'
+)
+
+//css
+body {
+  font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif; 
+}
+
+
+
+//another example
+swal({
+  title: "Are you sure?",
+  text: "Once deleted, you will not be able to recover this imaginary file!",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+})
+.then((willDelete) => {
+  if (willDelete) {
+    swal("Poof! Your imaginary file has been deleted!", {
+      icon: "success",
+    });
+  } else {
+    swal("Your imaginary file is safe!");
+  }
+});

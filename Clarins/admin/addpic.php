cb3b2d80e28cabad2db535b2b1380cd19c9a073d
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-25 14:16:35 +0700

    finish addpic

--- Clarins/admin/addpic.php
+++ Clarins/admin/addpic.php
@@ -12,42 +12,37 @@ if (isset($_FILES['image'])) {
     $extensions = array("jpeg", "jpg", "png");
 
     if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        header('location:addpic.php?'  . $_POST["productID"] . '&error');
     }
 
     if ($file_size > 2000000) {
-        $errors[] = 'File size must not bigger than 2 MB';
+        header('location:addpic.php?'  . $_POST["productID"] . '&error');
     }
 
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "../img/" . $file_name);
-        $sql = "SELECT pic1, pic2, pic3, pic4 from products where productID =" . $_GET["prod"];
-        $result = $conn->query($sql);
-        $prd = $result->fetch_array(MYSQLI_ASSOC);
-        $sql = "";
-        if (!isset($prd["pic1"])) :
-            $sql = "UPDATE products SET pic1='img/$file_name' WHERE products.productID =" . $_POST["productID"];
+    move_uploaded_file($file_tmp, "../img/" . $file_name);
+    $sql = "SELECT pic1, pic2, pic3, pic4 from products where productID =" . $_GET["prod"];
+    $result = $conn->query($sql);
+    $prd = $result->fetch_array(MYSQLI_ASSOC);
+    $sql = "";
+    if (!isset($prd["pic1"])) :
+        $sql = "UPDATE products SET pic1='img/$file_name' WHERE products.productID =" . $_POST["productID"];
+    else :
+        if (!isset($prd["pic2"])) :
+            $sql = "UPDATE products SET pic2='img/$file_name' WHERE products.productID =" . $_POST["productID"];
         else :
-            if (!isset($prd["pic2"])) :
-                $sql = "UPDATE products SET pic2='img/$file_name' WHERE products.productID =" . $_POST["productID"];
+            if (!isset($prd["pic3"])) :
+                $sql = "UPDATE products SET pic3='img/$file_name' WHERE products.productID =" . $_POST["productID"];
             else :
-                if (!isset($prd["pic3"])) :
-                    $sql = "UPDATE products SET pic3='img/$file_name' WHERE products.productID =" . $_POST["productID"];
+                if (!isset($prd["pic4"])) :
+                    $sql = "UPDATE products SET pic4='img/$file_name' WHERE products.productID =" . $_POST["productID"];
                 else :
-                    if (!isset($prd["pic4"])) :
-                        $sql = "UPDATE products SET pic4='img/$file_name' WHERE products.productID =" . $_POST["productID"];
-                    else :
-                        header("location:product.php?error");
-                    endif;
+                    header("location:product.php?error");
                 endif;
             endif;
         endif;
-        $result = $conn->query($sql);
-
-        header("location:addpic.php?prod=" . $_POST["productID"]);
-    } else {
-        print_r($errors);
-    }
+    endif;
+    $result = $conn->query($sql);
+    header("location:addpic.php?prod=" . $_POST["productID"] . "&success");
 }
 include("header.php");
 ?>
@@ -82,6 +77,30 @@ include("header.php");
     <section class="content">
         <div class="container-fluid">
             <div>
+                <?php
+                if (isset($_GET["success"])) {
+                    echo ('<div class="alert alert-success alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <h5><i class="icon fas fa-thumbs-up"></i> Alert!</h5>
+    File upload successfully!
+    </div>');
+                }
+                if (isset($_GET["error"])) {
+                    echo ('<div class="alert alert-danger alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+    File cannot be uploaded! Please check the requirement.
+    </div>');
+                }
+                ?>
+                <p>
+                    <b>REQUIREMENT:</b> Make sure you strickly follow:
+                </p>
+                <ul>
+                    <li>Only jpg/jpeg and png file are accepted</li>
+                    <li>File size should be smaller than 2MB</li>
+                    <li>Filename cannot have any special chars</li>
+                </ul>
                 <form action="" class="row" method="POST" enctype="multipart/form-data">
                     <input type="text" name="productID" value="<?php echo ($_GET["prod"]); ?>" class="d-none">
                     <div class="form-group col-7">

4a36bff9355af48cd3a35d5a54983595572753ea
phuopham <108047552+phuopham@users.noreply.github.com>
2022-08-27 14:01:11 +0700

    Update discount.php

--- Clarins/discount.php
+++ Clarins/discount.php
@@ -17,14 +17,14 @@ $discounts = $result->fetch_all(MYSQLI_ASSOC);
                 <div class="owl-carousel team-carousel">
                     <?php foreach ($discounts as $id => $discount) :
                         $pricedc = $discount["price"] - ($discount["price"] * ($discount["discount"] / 100)); ?>
-                        <div class="team-item">
+                        <div class="team-item ">
                             <div class="team-img mx-auto">
                                 <img class="rounded-circle w-100 h-100" src="<?php echo ($discount["pic1"]) ?>" style="object-fit: cover;">
                             </div>
-                            <div class="position-relative text-center bg-light rounded px-4 py-5" style="margin-top: -100px;">
+                            <div class="position-relative text-center bg-light rounded px-4 py-5 d-flex flex-column justify-content-between " style="height:32em; margin-top: -100px;">
                                 <h3 class="font-weight-bold mt-5 mb-3 pt-5"><?php echo ($discount["name"]) ?></h3>
                                 <h3 class="font-weight-bold mb-3 text-primary">Only $<?php echo ($pricedc) ?> </h3>
-                                <h6 class="text-muted mb-4 mt-auto"><?php echo (substr($discount["description"], 0, 100)) ?></h6>
+                                <h6 class="text-muted mb-4"><?php echo (substr($discount["description"], 0, 100)) ?></h6>
                                 <div class="d-flex justify-content-center pt-1">
                                     <a href="detail.php?prod=<?php echo ($discount["productID"]) ?>" class=" btn btn-sm btn-secondary" style="z-index:2;">Order Now</a>
                                 </div>
